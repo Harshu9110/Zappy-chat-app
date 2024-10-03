aba11456ba4042c668304746809cb5fa955444bd
Harsh Srivastava <harshsrivastavasanu2003@gmail.com>
2024-10-03 21:17:27 +0530

    added backend and frontend url

--- server/index.js
+++ server/index.js
@@ -66,7 +66,7 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://zappy-chat-app.vercel.app",
     methods: ["GET", "POST"],
   },
 });
